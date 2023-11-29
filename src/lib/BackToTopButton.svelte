747fcb65bf409a8a7da11aa1fa54c55646afdd75
admin <ahannon228@gmail.com>
2023-11-29 16:54:18 -0500

    Changed BackToTopButton to use a tag instead of onClick function.

--- src/lib/BackToTopButton.svelte
+++ src/lib/BackToTopButton.svelte
@@ -1,14 +1,11 @@
 <script>
 	import IconUpCaret from './icons/IconUpCaret.svelte';
-
-	function toTop() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
 </script>
 
-<button class="fixed z-10 left-1/2 top-0 h-[40px] w-[40px] -translate-x-1/2" on:click={toTop}>
-	<div class=" m-1 w-9 fill-neutral-700 hover:animate-jump hover:animate-duration-1000 hover:animate-infinite">
-		<IconUpCaret />
-	</div>
-</button>
+<a href="/">
+	<button class="fixed left-1/2 top-0 z-10 h-[40px] w-[40px] -translate-x-1/2">
+		<div class=" m-1 w-9 fill-neutral-700 hover:animate-jump hover:animate-duration-1000 hover:animate-infinite">
+			<IconUpCaret />
+		</div>
+	</button>
+</a>
